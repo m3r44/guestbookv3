8b6601b6026359c8a1b1aea2933a6215a4e92d38
nurmahirah <merahgottheworld@gmail.com>
2019-03-05 17:51:57 +0800

    added redirect & pagination btn w/o flash message

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(__DIR__);
-$twig = new Twig_Environment($loader,  ['post' => $_POST]); #['debug' => true],
+$twig = new Twig_Environment($loader, ['debug' => true], ['post' => $_POST]); #
 //$twig->addGlobal('session', $_SESSION);
 $twig->addExtension(new \Twig\Extension\DebugExtension());
 date_default_timezone_set('Asia/Kuala_Lumpur');
 
+session_start();
 $conn = new mysqli("localhost", "root", "root");
 mysqli_select_db($conn, "guestbook") or die (mysqli_error($conn));
 $sql_display = mysqli_query($conn, "SELECT * FROM guestbook ORDER BY id DESC");
@@ -17,7 +18,18 @@ $is_post_delete = false;
 $result_add_post = false;
 $result_delete_post = false;
 
+function redirect_to( $location ) {
+    if ($location != NULL) {
+        header("Location: {$location}");
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 if ($_POST) {
+    //echo "<meta http-equiv='refresh' content='0'>";
     # Condition to add post
     if (isset($_POST['postbtn'])) {
         $is_post_add = true;
@@ -28,12 +40,11 @@ if ($_POST) {
         if ($name && $email && $message) {
             $time = date("h:i A");
             $date = date("F d, Y");
+
             $result_add_post = mysqli_query($conn,
                 "INSERT INTO guestbook (name, email, message, time, date) VALUES 
                             ('$name', '$email', '$message', '$time', '$date')");
-            //window.location.reload();
-            //return $this->redirectToRoute('index');
-            //return $this->render('/guestbookv3/index.html.twig');;
+
         }
     }
 
@@ -43,7 +54,7 @@ if ($_POST) {
         $id = $_POST['id'];
         $result_delete_post = mysqli_query($conn, "DELETE FROM guestbook WHERE id = $id");
 
-        //echo $twig->render('index.html.twig', array($result_delete_post));
+
     }
 
     # Condition to edit post
@@ -53,12 +64,12 @@ if ($_POST) {
         mysqli_query($conn, "UPDATE guestbook SET message = '$message' WHERE id = $id");
 
     }
+    $if_redirect = redirect_to('index.php');
 }
 
 while ($results = mysqli_fetch_assoc($sql_display)) {
     $row[] = $results;
 }
-
 
 echo $twig->render(
     'index.html.twig', array(
